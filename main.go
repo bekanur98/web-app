aea805c9f1eba7604e2cf889a2e7d5c1555f4b7c
Beknur <bekanur98@bk.ru>
2023-01-30 22:27:38 +0600

    Custom Routing

--- main.go
+++ main.go
@@ -16,9 +16,16 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:bekanur98@gmail.com\">bekanur98@gmail.com</a></p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		homeHandler(w, r)
+	} else if r.URL.Path == "/contact" {
+		contactHandler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
