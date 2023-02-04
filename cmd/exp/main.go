74903a7bee1f7b07a7b61a891997b02314fe1d81
Beknur <bekanur98@bk.ru>
2023-02-04 11:52:48 +0600

    Added a contextual encoding

---
+++ cmd/exp/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Bio  string
+	Age  int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "John Smith",
+		Bio:  "Bio",
+		Age:  42,
+	}
+
+	err = t.Execute(os.Stdout, user)
+}
